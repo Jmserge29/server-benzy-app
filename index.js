32dd801d948ebd835203d4bc79174c9d03177914
Jmserge29 <Jmserge29@gmail.com>
2022-12-20 14:34:08 -0500

    change cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use('/User', routerUser)
 app.use('/Materia', routerMaterias)
 app.use('/Assignation', routerAssignation)
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({ origin: "http://localhost:3000", credentials: true}))
 
 // Server running
 app.listen(process.env.PORT_SERVER || 8089, async()=>{
