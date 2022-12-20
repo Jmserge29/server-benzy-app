8f6c7a366b50cff6b11bd3416791d7ec4d920d1e
Jmserge29 <Jmserge29@gmail.com>
2022-12-20 14:52:25 -0500

    modificed cors and app.use in the proyect frontend

--- index.js
+++ index.js
@@ -19,13 +19,14 @@ const app = express()
 dotenv.config()
 
 // App Uses
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/User', routerUser)
 app.use('/Materia', routerMaterias)
 app.use('/Assignation', routerAssignation)
-app.use(cors({ origin: "http://localhost:3000", credentials: true}))
 
 // Server running
 app.listen(process.env.PORT_SERVER || 8089, async()=>{
